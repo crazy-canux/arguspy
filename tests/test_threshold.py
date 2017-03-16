071afc5d3f3ead8968db09a599d2652720753829
Canux CHENG <canuxcheng@gmail.com>
2017-03-15 23:26:30 -0400

    [20170316]update.

---
+++ tests/test_threshold.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+test_threshold.py
+
+Copyright (C) 2017 Canux CHENG.
+All rights reserved.
+
+LICENSE GNU General Public License v3.0.
+
+:author: Canux CHENG canuxcheng@gmail.com
+:version: V1.0.0.0
+:since: Mon 13 Mar 2017 04:18:48 AM EDT
+
+DESCRIPTION:
+    test function threshold(result).
+"""
+import pytest
+
+
