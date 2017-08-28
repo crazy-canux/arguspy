e3f56075814587ac2d522c0cd6736e104f29180d
Canux CHENG <canux.cheng@emc.com>
2017-08-28 06:12:34 -0400

    add snmp

---
+++ arguspy/snmp_pysnmp.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+SUMMARY snmp_pysnmp.py
+
+Copyright (C) 2017 Canux CHENG.
+All rights reserved.
+
+LICENSE GNU General Public License v3.0.
+
+:author: Canux CHENG canuxcheng@gmail.com
+:version: 0.0.1
+:since: Thu 27 Jul 2017 10:15:38 AM EDT
+
+DESCRIPTION:
+"""
+import pysnmp
+
+from monitor import Monitor
+
+
+class Snmp(Monitor):
+
+    "Wrapper pysnmp for monitoring plugins."
+
+    def __init__(self):
+        super(Snmp, self).__init__()
+        self.logger.debug("Init snmp")
